6f5f50a91a3fcbc8489f3997b31e84bd380032bf
Pablo <berlanpa@gmail.com>
2025-08-09 21:33:50 +0200

    Add WritingPost type and sample posts to book.ts for writing content management

--- src/data/book.ts
+++ src/data/book.ts
@@ -1,1 +1,31 @@
-export * from '../../../src/data/book'
+export type WritingPost = {
+    title: string;
+    date: string;
+    url: string;
+    external: boolean;
+    description?: string;
+    image?: string;
+  };
+  
+  export const posts: WritingPost[] = [
+    { "title": "Preface To My Spiritual Writing", "date": "June 30, 2024", "url": "/writing/preface-to-my-spiritual-writing", "external": false },
+    { "title": "Who Are You Really", "date": "June 18, 2024", "url": "/writing/who-are-you-really", "external": false },
+    { "title": "You And Your Friend's Art", "date": "June 17, 2024", "url": "/writing/you-and-your-friends-art", "external": false },
+    { "title": "Fear", "date": "May 4, 2024", "url": "/writing/fear", "external": false },
+    { "title": "Choose Your Games Wisely", "date": "November 26, 2023", "url": "/writing/games", "external": false },
+    { "title": "The Breaking Point of Ambition", "description": "Why \"pragmatism\" often triumphs over ambition as we get older", "date": "January 4, 2023", "url": "/writing/ambition", "external": false },
+    { "title": "A Framework for Building Technical Skills", "date": "June 3, 2021", "image": "https://khazar.org/uploads/files/engineering/images/1m.jpg", "url": "https://medium.com/@adam-maj/a-proven-framework-for-efficiently-building-technical-competence-in-any-field-c3131c27d89f", "external": true },
+    { "title": "DMT & The Brain: The Alien World Theory", "date": "May 3, 2021", "image": "https://miro.medium.com/max/1400/0*Ah9RakYvHZXkpQ_G.webp", "url": "https://medium.com/@adam-maj/dmt-the-brain-building-alien-worlds-8be4be5efe85", "external": true },
+    { "title": "The Truth in the Irrational", "description": "Why some of the most important truths lie in the things we are most skeptical of", "image": "https://substackcdn.com/image/fetch/w_848,c_limit,f_webp,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2F3bb4eaca-e7f8-4efe-8bd4-c0384d4dff1e_1110x555.jpeg", "date": "May 1, 2021", "url": "/writing/irrational", "external": false },
+    { "title": "The Natural Stepping Stone Toward Awareness & Existentialism", "description": "Why nihilism is often the first step toward personal philosophical growth", "image": "http://i.gzn.jp/img/2017/07/28/optimistic-nihilism/cap00002.png", "date": "April 1, 2021", "url": "/writing/nihilism", "external": false },
+    { "title": "A Thought Experiment on Perception and Non-Dualism", "description": "Exploring non-duality through the subjective experience of senses", "image": "https://www.diarioeconomico.co.mz/wp-content/uploads/2022/12/psychedelic-treatments_Easy-Resize.com_.jpg", "date": "March 18, 2021", "url": "/writing/non-dualism", "external": false },
+    { "title": "The Importance of Open Mindedness in Politics", "description": "Why politics can't be as black and white as we often make it out to be", "date": "March 13, 2021", "url": "/writing/politics", "external": false },
+    { "title": "An Unconventional Perspective on Happiness and Meaning", "description": "Thoughts from younger me on what defines meaning in life", "image": "https://kurzgesagt.org/wp-content/uploads/2018/07/inanutshell-kurzgesagt-optimistic-nihilism-04.png", "date": "February 4, 2021", "url": "/writing/meaning", "external": false },
+    { "title": "The Secret to Rapid Learning in the Age of Technology", "date": "May 9, 2020", "url": "https://medium.com/@adam-maj/meta-learning-the-secret-to-rapid-learning-and-growth-in-the-age-of-technology-eea8693ae3a3", "external": true },
+    { "title": "How to Increase Motivation and Take Back Control of Your Life", "date": "May 2, 2020", "image": "https://miro.medium.com/max/1400/1*c8XpYy-vbpyBIbeHgErw8A.webp", "url": "https://medium.com/@adam-maj/increasing-internal-motivation-and-getting-back-in-control-of-yourself-ff171a2bbb87", "external": true },
+    { "title": "The Ultimate Guide to Learning Artificial Intelligence", "date": "April 30, 2020", "image": "https://miro.medium.com/max/1400/1*PvjBTPY5KvtJoSoCUipzWw.webp", "url": "https://medium.com/@adam-maj/the-ultimate-guide-to-becoming-an-artificial-intelligence-expert-db5124dc8ae0", "external": true },
+    { "title": "The Enemy of Progress", "date": "April 10, 2020", "url": "https://medium.com/@adam-maj/the-enemy-of-progress-what-is-complacency-and-how-can-we-avoid-it-5e988cafa1e8", "external": true }
+  ];
+  
+  
+  
